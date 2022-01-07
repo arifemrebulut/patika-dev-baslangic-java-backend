c0ebd3aeda935f31f2d138da1d493dfb965c6479
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-01-07 16:47:08 +0300

    create AsalSayilarRecursive

--- src/AsalSayilarRecursive.java
+++ src/AsalSayilarRecursive.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class AsalSayilarRecursive {
+import java.util.Scanner;
+
+public class AsalSayilarRecursive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+        System.out.println("Bir Sayı Giriniz: ");
+        inputNumber = scanner.nextInt();
+
+        asal(inputNumber, inputNumber / 2);
+    }
+
+    public static void asal(int number, int i){
+        if (i == 1) {
+            System.out.print("Sayı asaldır.");
+        } else {
+            if (number % i == 0){
+                System.out.println("Asal değildir.");
+            }
+            else{
+                asal(number, i - 1);
+            }
+        }
+    }
 }
